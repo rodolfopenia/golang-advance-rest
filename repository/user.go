ee19bdea5fdf37573db1eb9ebc3ecf86568a4a13
rodolfopenia <rodolfo.penia@hotmail.com>
2024-04-14 23:33:18 -0500

    Patron Repository

---
+++ repository/user.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/rodolfopenia/go-advance-rest/models"
+)
+
+type UserRepository interface {
+	InsertUser(ctx context.Context, user *models.User) error
+	GetUserById(ctx context.Context, id int64) (*models.User, error)
+}
+
+// El codigo debe basarse en abstracciones
+// y no en cosas concretas
+
+var implementation UserRepository
+
+func SetRepository(repository UserRepository) {
+	implementation = repository
+}
+
+func InsertUser(ctx context.Context, user *models.User) error {
+	return implementation.InsertUser(ctx, user)
+}
+
+func GetUserById(ctx context.Context, id int64) (*models.User, error) {
+	return implementation.GetUserById(ctx, id)
+}
