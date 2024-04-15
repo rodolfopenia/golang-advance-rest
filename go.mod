120be9982c449d80e3888e0f912324940f4cbf26
rodolfopenia <rodolfo.penia@hotmail.com>
2024-04-15 00:37:36 -0500

    Docker file, sql up y handler

--- go.mod
+++ go.mod
@@ -7,5 +7,7 @@ require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
 	golang.org/x/net v0.17.0 // indirect
 )
---
+++ handlers/user.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/rodolfopenia/go-advance-rest/models"
+	"github.com/rodolfopenia/go-advance-rest/repository"
+	"github.com/rodolfopenia/go-advance-rest/server"
+	"github.com/segmentio/ksuid"
+)
+
+type SignUpRequest struct {
+	Emil     string `json:"email"`
+	Password string `json:"password"`
+}
+
+type SignUpResponse struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+}
+
+func SignUpHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var request = SignUpRequest{}
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		id, err := ksuid.NewRandom()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		var user = models.User{
+			Email:    request.Emil,
+			Password: request.Password,
+			Id:       id.String(),
+		}
+
+		err = repository.InsertUser(r.Context(), &user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(SignUpResponse{
+			Id:    user.Id,
+			Email: user.Email,
+		})
+	}
+}
