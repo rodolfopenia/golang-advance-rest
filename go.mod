aed657a73912b6b15180e9aef4b864551af9f8e0
rodolfopenia <rodolfo.penia@hotmail.com>
2024-04-29 01:38:59 -0500

    Create frontend for test apis and websockets

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/rs/cors v1.11.0 // indirect
 	github.com/segmentio/ksuid v1.0.4 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
--- server/server.go
+++ server/server.go
@@ -10,6 +10,7 @@ import (
 	"github.com/rodolfopenia/go-advance-rest/database"
 	"github.com/rodolfopenia/go-advance-rest/repository"
 	"github.com/rodolfopenia/go-advance-rest/websocket"
+	"github.com/rs/cors"
 )
 
 type Config struct {
@@ -60,6 +61,7 @@ func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
 	b.router = mux.NewRouter()
 	binder(b, b.router)
+	handler := cors.Default().Handler(b.router)
 	repo, err := database.NewPostgresRepository(b.config.DatabaseUrl)
 	if err != nil {
 		log.Fatal(err)
@@ -67,7 +69,7 @@ func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
 	go b.hub.Run()
 	repository.SetRepository(repo)
 	log.Println("Starting server on port", b.Config().Port)
-	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+	if err := http.ListenAndServe(b.config.Port, handler); err != nil {
 		log.Println("ListenAndServe: ", err)
 	} else {
 		log.Fatalf("server stopped")
